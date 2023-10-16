dbe13ac70dbd9348ee5a9cef7a0ac17305b79dbb
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-10-16 13:59:09 +0000

    delete files

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-
-int main(void) {
-    int chars_printed = _printf("Hello, %c! This is a %s example.%%\n", 'W', "printf");
-    _printf("%c", 'S');
-    printf("Total characters printed: %d\n", chars_printed);
-    return 0;
-}
