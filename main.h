b846558e7f0171872716d1b692c917f976382af9
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-10-17 16:25:24 +0000

    sorry

--- main.h
+++ main.h
@@ -16,15 +16,6 @@ typedef struct fmt
         int (*fn)(va_list);
 } fmt_t;
 
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-#define S_LONG 2
-#define S_SHORT 1
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_SPACE 16
-
 int _strlen(char *str);
 int _putchar(char c);
 int handle_print(const char *fmt, int *ind, va_list list);
@@ -32,15 +23,6 @@ int print_char(va_list list);
 int print_string(va_list list);
 int print_percent(va_list list);
 int _printf(const char *format, ...);
-
-int specifier(va_list arg, char buff[],
-	int flags, int width, int precision, int size);
-long int convert_size_n(long int num, int size);
-int write_number(int is_negative, int ind, char buffer[],
-	int flags, int width, int precision, int size);
-int write_num(int ind, char buff[],
-        int flags, int width, int prec,
-        int length, char padd, char extra);
 
 #endif
 
