47ff105b9445877747cf98f830fd6536e57e85fc
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-10-20 08:20:02 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,49 +1,39 @@
-#include "main.h"
 
 /**
- * handle_print - Print formatted output based on format specifiers.
+ * handle_print – Print formatted output based on format specifiers.
  * @fmt: The format string.
  * @ind: Index into the format string.
  * @list: The va_list of arguments.
  * Return: The number of characters printed.
  */
-
-int handle_print(const char *fmt, int *ind, va_list list)
-{
+int handle_print(const char *fmt, int *ind, va_list list) {
 int printed_chars = -1;
 int i;
 
-fmt_t fmt_types[] = {{'c', print_char}, {'s', print_string},
-{'%', print_percent}, {'b', print_binary}, {'i', print_int},
-	{'d', print_decimal}, {'o', print_octal}, {'u', print_unsigned},
-	{'x', print_hex}, {'X', print_hex_upper}, {'\0', NULL}};
+fmt_t fmt_types[] = {
+        {'c', print_char}, {'s', print_string}, {'%', print_percent}, {'b', print_binary},
+        {'d', print_int}, {'i', print_int}, {'o', print_octal}, {'u', print_unsigned},
+        {'x', print_hex}, {'X', print_hex_upper}};
 
 for (i = 0; fmt_types[i].fmt != '\0'; i++)
 {
-if (fmt[(*ind)] == fmt_types[i].fmt)
-{
-return (fmt_types[i].fn(list));
+if (fmt[(*ind)] == fmt_types[i].fmt) return fmt_types[i].fn(list);
 }
 }
 
-if (fmt_types[i].fmt == '\0')
-{
-if (fmt[(*ind)] == '\0')
-{
+if (fmt_types[i].fmt == '\0') {
+if (fmt[(*ind)] == '\0') {
 return (-1);
 }
-
 _putchar('%');
-
-if (fmt[(*ind) - 1] == ' ')
-{
+if (fmt[(*ind) - 1] == ' ') {
 _putchar(' ');
 }
 else
 {
 _putchar(fmt[(*ind)]);
+}
 return (2);
 }
-}
-return (printed_chars);
+return printed_chars;
 }
