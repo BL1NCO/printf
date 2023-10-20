e76b678fe3273b01877fa38fe8d29670054a9874
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-10-20 14:46:37 +0000

    fix

--- handle_print.c
+++ handle_print.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 /**
- * handle_print – Print formatted output based on format specifiers.
+ * handle_print - Identify and process format specifiers in a format string.
+ *
+ * This function scans the format string for format
+ * specifiers starting with '%',
+ * matches them with corresponding handling functions,
+ * and processes the associated arguments.
+ *
  * @fmt: The format string.
- * @ind: Index into the format string.
+ * @ind: Pointer to an index into the format string.
  * @list: The va_list of arguments.
- * Return: The number of characters printed.
+ *
+ * Return: The number of characters printed by the handling function.
  */
 int handle_print(const char *fmt, int *ind, va_list list)
 {
@@ -14,7 +21,7 @@ int i;
 
 fmt_t fmt_types[] = {
 {'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'b', print_binary}, {'d', print_int}, {'i', print_int}, {'o', print_octal}, 
+{'b', print_binary}, {'d', print_int}, {'i', print_int}, {'o', print_octal},
 {'u', print_unsigned}, {'x', print_hex}, {'X', print_hex_upper}};
 
 for (i = 0; fmt_types[i].fmt != '\0'; i++)
@@ -32,10 +39,8 @@ _putchar('%');
 if (fmt[(*ind) - 1] == ' ')
 _putchar(' ');
 else
-_putchar(fmt[(*ind)]);}
-return (2);
-
+_putchar(fmt[(*ind)]);
 }
+return (2);
 return (printed_chars);
 }
-
