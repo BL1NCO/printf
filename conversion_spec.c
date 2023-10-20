94fb7beb2c474c76fe7b5f6ed5ab41d608d22c76
khalil <labiadkhalil11@gmail.com>
2023-10-20 23:07:23 +0100

    sdqf

--- conversion_spec.c
+++ conversion_spec.c
@@ -7,18 +7,24 @@
  * Return: always 0
  */
 
-int conversion_spec(va_list args)
+int conversion_spec(va_list rev)
 {
 
-	char *str = va_arg(args, char*);
+	char *str = va_arg(rev, char*);
 	int g;
 	int i = 0;
 
 	if (str == NULL)
+	{
 		str = "(null)";
+	}
 	while (str[i] != '\0')
+	{
 		i++;
+	}
 	for (g = i - 1; g >= 0; g--)
+	{
 		_putchar(str[g]);
+	}
 	return (i);
 }
--- main.h
+++ main.h
@@ -36,6 +36,6 @@ int print_hex_upper(va_list n);
 int print_non_printable(va_list types);
 int append_hexa_code(char ascii_code, char buffer[], int i);
 int print_pointer(va_list types);
-int conversion_spec(va_list args);
+int conversion_spec(va_list rev);
 
 #endif
