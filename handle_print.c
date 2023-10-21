f0065241e25d9e4f7d0707d36eabf54b66d31063
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-10-21 03:00:26 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,48 +1,49 @@
 #include "main.h"
-
 /**
- * handle_print - Identify and process format specifiers in a format string.
- *
- * This function scans the format string for format
- * specifiers starting with '%',
- * matches them with corresponding handling functions,
- * and processes the associated arguments.
- *
- * @fmt: The format string.
- * @ind: Pointer to an index into the format string.
- * @list: The va_list of arguments.
- *
- * Return: The number of characters printed by the handling function.
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-int handle_print(const char *fmt, int *ind, va_list list)
-{
-int printed_chars = -1;
-int i;
-
-fmt_t fmt_types[] = {
-{'c', print_char}, {'s', print_string}, {'%', print_percent},
-{'b', print_binary}, {'d', print_int}, {'i', print_int}, {'o', print_octal},
-{'u', print_unsigned}, {'x', print_hex}, {'X', print_hex_upper},
-{'S', print_non_printable}, {'p', print_pointer}, {'r', conversion_spec}};
-
-for (i = 0; fmt_types[i].fmt != '\0'; i++)
-{
-if (fmt[(*ind)] == fmt_types[i].fmt)
-return (fmt_types[i].fn(list));
-}
-if (fmt_types[i].fmt == '\0')
-{
-if (fmt[(*ind)] == '\0')
-{
-return (-1);
-}
-_putchar('%');
-if (fmt[(*ind) - 1] == ' ')
-_putchar(' ');
-else
-_putchar(fmt[(*ind)]);
-}
-return (2);
-return (printed_chars);
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
 }
